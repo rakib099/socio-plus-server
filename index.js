e8fb55419d32f448618924bc8c6290a54ce7327d
Rakib099 <rakibnabi97@gmail.com>
2023-02-17 14:03:23 +0600

    Connected to MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,32 @@ const port = process.env.PORT || 5000;
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i9w8jvi.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const postCollection = client.db('SocioPlus').collection('posts');
+        app.get('/posts', async (req, res) => {
+            const query = {};
+            const cursor = postCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send('Socio Plus Server Running');
